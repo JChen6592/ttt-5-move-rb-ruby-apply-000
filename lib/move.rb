d0716a6363af1da4b61dada432db6e637d6ae994
John Chen <johnchen062094@gmail.com>
2020-04-25 22:28:21 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,7 @@ end
 # code your input_to_index and move method here!
 def input_to_index(move)
   index = move.to_i 
-  return index - 1
+  return index
 end
 
 board = [" "," "," "," "," "," "," "," "," "]
